6b79967c227a1837b23d0e84d6e2a35a1f510cc9
Johany Navarro <johany87@gmail.com>
2025-12-24 13:58:23 +0000

    Update

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -3,7 +3,7 @@ AGES = [20, 21, 22, 23]
 
 i = 0
 while i < len(NAMES):
-    print(NAMES[i], AGES[i])
+    print(f"{NAMES[i]} {AGES[i]}")
     i += 1
 
 for name in NAMES:
@@ -19,3 +19,5 @@ for i in range(5):
     print(i)
 
 # enumerate
+for index, (name, age) in enumerate(zip(NAMES, AGES)):
+    print(f"{index} - Name: {name} - age: {age}")
